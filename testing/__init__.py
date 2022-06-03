cd1fbaf407a28c911de4c3af34ed2055102d6071
Prabal Pathak <prabal01pathak@gmail.com>
2022-06-03 23:18:30 +0530

    add testing module

--- testing/__init__.py
+++ testing/__init__.py
@@ -1,1 +1,19 @@
+"""
+Description: init file for the package
+"""
 __version__ = '0.1.0'
+__author__ = 'Prabal Pathak'
+__email__ = 'prabal01pathak'
+__status__ = 'Development'
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2019 Prabal Pathak'
+__all__ = ['__version__', '__author__', '__email__', '__status__', '__license__', '__copyright__']
+
+def main():
+    """
+    Main function
+    """
+    return 0
+
+if __name__ == '__main__':
+    main()
